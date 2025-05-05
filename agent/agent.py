b5f51155d41188e44d8d0e242a591398ac6cce72
Ahmed Ayman <ahm.ayman4566@gmail.com>
2025-05-05 19:53:12 +0300

    Working Final version

--- agent/agent.py
+++ agent/agent.py
@@ -2,52 +2,65 @@ import sys
 import os
 from dotenv import load_dotenv
 from google import genai
-
+import json
 
 # Load the environment variables from .env file
 load_dotenv()
 api_key = os.environ.get('GEMINI_API_KEY')
 
-client = genai.Client(api_key=api_key)
+def generateReport(data):
+    try:
+        client = genai.Client(api_key=api_key)
+        config = {
+                    'temperature': 1.1,
+                    'responseMimeType': 'text/plain',
+                    'systemInstruction': '''
+                    You are a trading guru. Given data on share prices over the past 3 days, write a report of no more than 150 words describing the stocks performance and recommending whether to buy, hold or sell.
+                    ''',
+        }
+        model = 'gemini-2.0-flash'
+        contents = f'{data}'
+        
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents = contents,
+            config=config
+        )
+        agent = {}
+        agent['report'] = response.text
+        cur_path = os.path.dirname(__file__)
+        new_path = os.path.relpath('..\\data\\agent_report.json', cur_path)
+        
+        try:
+            os.remove(new_path)
+            print('previous report found and removed')
+        finally:
+            print('previous report not found')
+            with open(new_path, 'w') as json_file:
+                json.dump(agent, json_file, indent=4)             
+        print("Data saved to '\\Stock-Tracker-Web-Agent-Advisor\\data\\agent_report.json'")
+    except ValueError:
+        print('ValueError')
+    except IndexError:
+        print('IndexError') 
+generateReport('TSLA, AAPL')
 
-config = {
-            'temperature': 1.1,
-            'responseMimeType': 'text/plain',
-            'systemInstruction': '''
-            You are a trading guru. Given data on share prices over the past 3 days, write a report of no more than 150 words describing the stocks performance and recommending whether to buy, hold or sell.
-            Use examples provided between ### to set the style and tone of your response.
-            ###
-            OK baby, hold on tight! You are going to haate this! Over the past three days, Tesla (TSLA) shares have plummetted.
-            The stock opened at $223.98 and closed at $202.11 on the third day, with some jumping around in the meantime.
-            This is a great time to buy, baby! But not a great time to sell! But I'm not done! Apple (AAPL) stocks have gone stratospheric!
-            This is a seriously hot stock right now. They opened at $166.38 and closed at $182.89 on day three.
-            So all in all, I would hold on to Tesla shares tight if you already have them - they might bounce right back up and head to the stars!
-            They are volatile stock, so expect the unexpected. For APPL stock, how much do you need the money? Sell now and take the profits or hang on and wait for more!
-            If it were me, I would hang on because this stock is on fire right now!!! Apple are throwing a Wall Street party and y'all invited!
-            ###
-            ###
-            Apple (AAPL) is the supernova in the stock sky – it shot up from $150.22 to a jaw-dropping $175.36 by the close of day three.
-            We’re talking about a stock that’s hotter than a pepper sprout in a chilli cook-off, and it’s showing no signs of cooling down!
-            If you’re sitting on AAPL stock, you might as well be sitting on the throne of Midas.
-            Hold on to it, ride that rocket, and watch the fireworks, because this baby is just getting warmed up! Then there’s Meta (META), the heartthrob with a penchant for drama.
-            It winked at us with an opening of $142.50, but by the end of the thrill ride, it was at $135.90, leaving us a little lovesick.
-            It’s the wild horse of the stock corral, bucking and kicking, ready for a comeback. META is not for the weak-kneed So, sugar, what’s it going to be?
-            For AAPL, my advice is to stay on that gravy train. As for META, keep your spurs on and be ready for the rally.
-            ###
-            ''',
-}
-model = 'gemini-2.0-flash'
-contents = f'META, TSLA, AAPL'
-
-
-
-response = client.models.generate_content_stream(
-    model="gemini-2.0-flash",
-    contents = contents,
-    config=config
-)
-
-for chunk in response:
-    print(chunk.text, end="")
-
-#print(response.text)
+#Use examples provided between ### to set the style and tone of your response.
+####
+#OK baby, hold on tight! You are going to haate this! Over the past three days, Tesla (TSLA) shares have plummetted.
+#The stock opened at $223.98 and closed at $202.11 on the third day, with some jumping around in the meantime.
+#This is a great time to buy, baby! But not a great time to sell! But I'm not done! Apple (AAPL) stocks have gone stratospheric!
+#This is a seriously hot stock right now. They opened at $166.38 and closed at $182.89 on day three.
+#So all in all, I would hold on to Tesla shares tight if you already have them - they might bounce right back up and head to the stars!
+#They are volatile stock, so expect the unexpected. For APPL stock, how much do you need the money? Sell now and take the profits or hang on and wait for more!
+#If it were me, I would hang on because this stock is on fire right now!!! Apple are throwing a Wall Street party and y'all invited!
+####
+####
+#Apple (AAPL) is the supernova in the stock sky – it shot up from $150.22 to a jaw-dropping $175.36 by the close of day three.
+#We’re talking about a stock that’s hotter than a pepper sprout in a chilli cook-off, and it’s showing no signs of cooling down!
+#If you’re sitting on AAPL stock, you might as well be sitting on the throne of Midas.
+#Hold on to it, ride that rocket, and watch the fireworks, because this baby is just getting warmed up! Then there’s Meta (META), the heartthrob with a penchant for drama.
+#It winked at us with an opening of $142.50, but by the end of the thrill ride, it was at $135.90, leaving us a little lovesick.
+#It’s the wild horse of the stock corral, bucking and kicking, ready for a comeback. META is not for the weak-kneed So, sugar, what’s it going to be?
+#For AAPL, my advice is to stay on that gravy train. As for META, keep your spurs on and be ready for the rally.
+####
--- app/app.py
+++ app/app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template, jsonify, url_for ,request
+from flask import Flask, render_template, jsonify, url_for, request
 import subprocess
 import json
+import time
 import os
 import sys
 api_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'api'))
+agent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'agent'))
 sys.path.append(api_dir)
+sys.path.append(agent_dir)
 from stock_performance_fetcher import performanceFetcher
-
-#print(performanceFetcher(['TSLA']))
-
+from agent import generateReport
 
 app = Flask('Stock Advisor')
 
@@ -19,21 +20,20 @@ def index():
 @app.route("/data")
 def get_data():
     try:
-        # Construct the absolute path to data.json
-        app_dir = os.path.dirname(os.path.abspath(__file__)) # Get the directory of app.py
+        app_dir = os.path.dirname(os.path.abspath(__file__))
         data_path = os.path.join(app_dir, '..', 'data', 'data.json')
         with open(data_path, 'r') as f:
             data = json.load(f)
         return jsonify(data)
     except FileNotFoundError:
-        return jsonify({}) # Return empty if no data yet
+        return jsonify({})
 
 @app.route("/refresh", methods=['POST'])
 def refresh_data():
     try:
         scraper_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../scraper', 'scraper.py')
         subprocess.run(['python', scraper_path], check=True)
-        app_dir = os.path.dirname(os.path.abspath(__file__)) # Get the directory of app.py
+        app_dir = os.path.dirname(os.path.abspath(__file__))
         data_path = os.path.join(app_dir, '..', 'data', 'data.json')
         with open(data_path, 'r') as f:
             data = json.load(f)
@@ -43,13 +43,31 @@ def refresh_data():
         return jsonify({"error": "Failed to refresh data"}), 500
     except FileNotFoundError:
         return jsonify({})
-  
-  
-@app.route("/submit", methods=['GET'])
+
+
+@app.route("/submit", methods=['POST'])
 def submit_tickers():
-    print('\n\n\nsubmit_tickers\n\n\n')
-    tickers = request.args['selectedCompaniesList']
-    print(tickers)
+    #try:
+    #agent_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../agent', 'agent.py')
+    #subprocess.run(['python', agent_path], check=True)
+    data = request.get_json()
+    print('sending data to agent...')
+    generateReport(data)
+    print('generating report...')
+    app_dir = os.path.dirname(os.path.abspath(__file__))
+    data_path = os.path.join(app_dir, '..', 'data', 'agent_report.json')
+    with open(data_path, 'r') as f:
+        data = json.load(f)
+    f.close()
+    print('sending report...')
+    return jsonify(data['report'])
+
+    #except subprocess.CalledProcessError as e:
+        #print(f"Error running agent: {e}")
+        #return jsonify({"error": "Failed to connect to AI"}), 500
+    #except FileNotFoundError:
+        #return jsonify({})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
